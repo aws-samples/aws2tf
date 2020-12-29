b535c29c36e293ca6cdfb321edee2302e9377fd6
awsandy <awsandy@amazon.co.uk>
2020-12-29 19:20:47 +0000

    kms key skipping

--- scripts/080-get-kms-key.sh
+++ scripts/080-get-kms-key.sh
@@ -32,61 +32,72 @@ for c in `seq 0 0`; do
             else
                 cname=$(echo $awsout | jq -r ".${pref[(${c})]}[(${i})].${idfilt[(${c})]}")
             fi
-
-            echo "$ttft $cname"
-            fn=`printf "%s__k_%s.tf" $ttft $cname`
-            if [ -f "$fn" ] ; then
-                echo "$fn exists already skipping"
-                continue
-            fi
-            printf "resource \"%s\" \"k_%s\" {" $ttft $cname > $ttft.$cname.tf
-            printf "}" $cname >> $ttft.$cname.tf
-            printf "terraform import %s.%s %s" $ttft $cname $cname > import_$ttft_$cname.sh
-            terraform import $ttft.k_$cname "$cname" | grep Import
-            echo $?
-            #terraform state mv $ttft.$cname $ttft.k_$cname
-            terraform state show $ttft.k_$cname > t2.txt
-            tfa=`printf "%s.%s" $ttft k_$cname`
-            terraform show  -json | jq --arg myt "$tfa" '.values.root_module.resources[] | select(.address==$myt)' > $tfa.json
-            #echo $awsj | jq . 
-            rm $ttft.$cname.tf
-            # rename state to save problems later
-            cat t2.txt | perl -pe 's/\x1b.*?[mGKH]//g' > t1.txt
-            #	for k in `cat t1.txt`; do
-            #		echo $k
-            #	done
-            file="t1.txt"
-            echo $aws2tfmess > $fn
-            while IFS= read line
-            do
-				skip=0
-                # display $line or do something with $line
-                t1=`echo "$line"` 
-                if [[ ${t1} == *"="* ]];then
-                    tt1=`echo "$line" | cut -f1 -d'=' | tr -d ' '` 
-                    tt2=`echo "$line" | cut -f2- -d'='`
-                    if [[ ${tt1} == "arn" ]];then skip=1; fi                
-                    if [[ ${tt1} == "id" ]];then skip=1; fi          
-                    if [[ ${tt1} == "role_arn" ]];then skip=1;fi
-                    if [[ ${tt1} == "owner_id" ]];then skip=1;fi
-                    if [[ ${tt1} == "key_id" ]];then skip=1;fi
-                    #if [[ ${tt1} == "availability_zone" ]];then skip=1;fi
-                    if [[ ${tt1} == "kms:"* ]]; then
-                        tt2=`echo $tt2 | tr -d '"'`
-                        t1=`printf "\"%s\" = \"%s\"" $tt1 $tt2`
-                    fi
-
-                # else
-                    #              
+            keyd=`$AWS kms describe-key --key-id $cname`
+            keystate=$(echo $keyd | jq -r .KeyMetadata.KeyState)
+            keyman=$(echo $keyd | jq -r .KeyMetadata.KeyManager)
+            if [ "$keystate" == "Enabled" ]; then
+                echo "$ttft $cname"
+                fn=`printf "%s__k_%s.tf" $ttft $cname`
+                if [ -f "$fn" ] ; then
+                    echo "$fn exists already skipping"
+                    continue
                 fi
-
-                if [ "$skip" == "0" ]; then
-                    #echo $skip $t1
-                    echo $t1 >> $fn
-                fi
-                
-            done <"$file"
+                printf "resource \"%s\" \"k_%s\" {" $ttft $cname > $ttft.$cname.tf
+                printf "}" $cname >> $ttft.$cname.tf
+                printf "terraform import %s.%s %s" $ttft $cname $cname > import_$ttft_$cname.sh
+                terraform import $ttft.k_$cname "$cname"
+                if [ $? -eq 0 ]; then
             
+                    
+                    #terraform state mv $ttft.$cname $ttft.k_$cname
+                    terraform state show $ttft.k_$cname > t2.txt
+                    tfa=`printf "%s.%s" $ttft k_$cname`
+                    terraform show  -json | jq --arg myt "$tfa" '.values.root_module.resources[] | select(.address==$myt)' > $tfa.json
+                    #echo $awsj | jq . 
+                    rm $ttft.$cname.tf
+                    # rename state to save problems later
+                    cat t2.txt | perl -pe 's/\x1b.*?[mGKH]//g' > t1.txt
+                    #	for k in `cat t1.txt`; do
+                    #		echo $k
+                    #	done
+                    file="t1.txt"
+                    echo $aws2tfmess > $fn
+                    while IFS= read line
+                    do
+                        skip=0
+                        # display $line or do something with $line
+                        t1=`echo "$line"` 
+                        if [[ ${t1} == *"="* ]];then
+                            tt1=`echo "$line" | cut -f1 -d'=' | tr -d ' '` 
+                            tt2=`echo "$line" | cut -f2- -d'='`
+                            if [[ ${tt1} == "arn" ]];then skip=1; fi                
+                            if [[ ${tt1} == "id" ]];then skip=1; fi          
+                            if [[ ${tt1} == "role_arn" ]];then skip=1;fi
+                            if [[ ${tt1} == "owner_id" ]];then skip=1;fi
+                            if [[ ${tt1} == "key_id" ]];then skip=1;fi
+                            #if [[ ${tt1} == "availability_zone" ]];then skip=1;fi
+                            if [[ ${tt1} == "kms:"* ]]; then
+                                tt2=`echo $tt2 | tr -d '"'`
+                                t1=`printf "\"%s\" = \"%s\"" $tt1 $tt2`
+                            fi
+
+                        # else
+                            #              
+                        fi
+
+                        if [ "$skip" == "0" ]; then
+                            #echo $skip $t1
+                            echo $t1 >> $fn
+                        fi
+                        
+                    done <"$file"
+                else
+                    echo "Import Failed $ttft $cname"
+                    mv $ttft.$cname.tf $ttft.$cname.tf.failed
+                fi 
+            else
+                echo "$ttft $cname key state = $keystate"
+            fi  
         done
 
     fi

de21074e485191096d912b61e99d1c60498da556
awsandy <awsandy@amazon.co.uk>
2021-05-07 19:55:01 +0100

    up

--- aws2tf.sh
+++ aws2tf.sh
@@ -185,6 +185,7 @@ if [ "$t" == "appmesh" ]; then pre="360*"; fi
 if [ "$t" == "kms" ]; then pre="08*"; fi
 if [ "$t" == "lambda" ]; then pre="700*"; fi
 if [ "$t" == "rds" ]; then pre="60*"; fi
+if [ "$t" == "emr" ]; then pre="37*"; fi
 
 pwd
 if [ "$c" == "no" ]; then
---
+++ scripts/370-get-emr-cluster.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+if [ "$1" != "" ]; then
+    cmd[0]="$AWS emr list-clusters --active" 
+else
+    cmd[0]="$AWS emr list-clusters --active"
+fi
+
+pref[0]="Clusters"
+tft[0]="aws_emr_cluster"
+idfilt[0]="Id"
+
+#rm -f ${tft[0]}.tf
+
+for c in `seq 0 0`; do
+    
+    cm=${cmd[$c]}
+	ttft=${tft[(${c})]}
+	#echo $cm
+    awsout=`eval $cm`
+    count=`echo $awsout | jq ".${pref[(${c})]} | length"`
+    if [ "$count" -gt "0" ]; then
+        count=`expr $count - 1`
+        for i in `seq 0 $count`; do
+            #echo $i
+            cname=`echo $awsout | jq ".${pref[(${c})]}[(${i})].${idfilt[(${c})]}" | tr -d '"'`
+            echo "$ttft $cname"
+            fn=`printf "%s__%s.tf" $ttft $cname`
+            if [ -f "$fn" ] ; then
+                echo "$fn exists already skipping"
+                continue
+            fi
+            printf "resource \"%s\" \"%s\" {" $ttft $cname > $ttft.$cname.tf
+            printf "}" $cname >> $ttft.$cname.tf
+            printf "terraform import %s.%s %s" $ttft $cname $cname > data/import_$ttft_$cname.sh
+            terraform import $ttft.$cname "$cname" | grep Import
+            terraform state show $ttft.$cname > t2.txt
+            tfa=`printf "data/%s.%s" $ttft $cname`
+            terraform show  -json | jq --arg myt "$tfa" '.values.root_module.resources[] | select(.address==$myt)' > $tfa.json
+            #echo $awsj | jq . 
+            rm $ttft.$cname.tf
+            cat t2.txt | perl -pe 's/\x1b.*?[mGKH]//g' > t1.txt
+            #	for k in `cat t1.txt`; do
+            #		echo $k
+            #	done
+            file="t1.txt"
+            echo $aws2tfmess > $fn
+            while IFS= read line
+            do
+				skip=0
+                # display $line or do something with $line
+                t1=`echo "$line"` 
+                if [[ ${t1} == *"="* ]];then
+                    tt1=`echo "$line" | cut -f1 -d'=' | tr -d ' '` 
+                    tt2=`echo "$line" | cut -f2- -d'='`
+                    if [[ ${tt1} == "arn" ]];then skip=1; fi                
+                    if [[ ${tt1} == "id" ]];then skip=1; fi          
+                    if [[ ${tt1} == "role_arn" ]];then skip=1;fi
+                    if [[ ${tt1} == "owner_id" ]];then skip=1;fi
+                    if [[ ${tt1} == "resource_owner" ]];then skip=1;fi
+                    if [[ ${tt1} == "cluster_state" ]];then skip=1;fi
+                    if [[ ${tt1} == "master_public_dns" ]];then skip=1;fi
+                    if [[ ${tt1} == "realm" ]];then 
+                    echo "kdc_admin_password = \"CHANGE-ME\"" >> $fn
+                    fi
+                    #if [[ ${tt1} == "availability_zone" ]];then skip=1;fi
+                    if [[ ${tt1} == "last_updated_date" ]];then skip=1;fi
+                    if [[ ${tt1} == "vpc_id" ]]; then
+                        tt2=`echo $tt2 | tr -d '"'`
+                        t1=`printf "%s = aws_vpc.%s.id" $tt1 $tt2`
+                    fi
+               
+                fi
+                if [ "$skip" == "0" ]; then
+                    #echo $skip $t1
+                    echo $t1 >> $fn
+                fi
+                
+            done <"$file"
+
+            ../../scripts/get-emr-inst-group.sh $cname
+
+        done
+
+    fi
+done
+
+rm -f t*.txt
+

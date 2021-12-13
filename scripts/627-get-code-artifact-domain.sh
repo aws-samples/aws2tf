2f5509ab0f33a34066b59c5a70191d34bf637bc6
awsandy <awsandy@amazon.co.uk>
2021-12-13 19:19:23 +0000

    artifact start

--- scripts/627-get-code-artifact-domain.sh
+++ scripts/627-get-code-artifact-domain.sh
@@ -28,6 +28,7 @@ for c in `seq 0 0`; do
             #echo $i
             cname=`echo $awsout | jq ".${pref[(${c})]}[(${i})].${idfilt[(${c})]}" | tr -d '"'`
             aarn=`echo $awsout | jq ".${pref[(${c})]}[(${i})].arn" | tr -d '"'`
+            
             echo "$ttft $cname"
             fn=`printf "%s__%s.tf" $ttft $cname`
             if [ -f "$fn" ] ; then

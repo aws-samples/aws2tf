d2a87b833ad360b495974070be189fdf43129814
awsandy <awsandy@amazon.co.uk>
2021-12-23 14:52:50 +0000

    up

--- scripts/255-get-ec2-launch-templates.sh
+++ scripts/255-get-ec2-launch-templates.sh
@@ -28,11 +28,16 @@ for c in `seq 0 0`; do
             #echo $i
             cname=`echo $awsout | jq ".${pref[(${c})]}[(${i})].${idfilt[(${c})]}" | tr -d '"'`
             echo "$ttft $cname"
-            printf "resource \"%s\" \"%s\" {" $ttft $cname > $ttft.$cname.tf
-            printf "}" >> $ttft.$cname.tf
+            fn=`printf "%s__%s.tf" $ttft $cname`
+            if [ -f "$fn" ] ; then echo "$fn exists already skipping" && continue; fi
+
+
+
+            printf "resource \"%s\" \"%s\" {" $ttft $cname > $fn
+            printf "}" >> $fn
             terraform import $ttft.$cname "$cname" | grep Import
             terraform state show $ttft.$cname > t2.txt
-            rm $ttft.$cname.tf
+            rm -f $fn
             cat t2.txt | perl -pe 's/\x1b.*?[mGKH]//g' > t1.txt
             #	for k in `cat t1.txt`; do
             #		echo $k
@@ -41,7 +46,7 @@ for c in `seq 0 0`; do
             ud=`$AWS ec2 describe-launch-template-versions --launch-template-id $cname  | jq .LaunchTemplateVersions[0].LaunchTemplateData.UserData | tr -d '"' | base64 --decode`          
             $AWS ec2 describe-launch-template-versions --launch-template-id $cname | jq .LaunchTemplateVersions[0].LaunchTemplateData.UserData | tr -d '"' | base64 --decode > $cname.sh
             file="t1.txt"
-            fn=`printf "%s__%s.tf" $ttft $cname`
+
             echo $aws2tfmess > $fn
             sgid=""
             while IFS= read line

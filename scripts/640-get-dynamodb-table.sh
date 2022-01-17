07425b8eca1cee72d7af180f9b7640f749bcbcd8
awsandy <awsandy@amazon.co.uk>
2022-01-17 13:28:32 +0000

    dynamo debug

--- scripts/640-get-dynamodb-table.sh
+++ scripts/640-get-dynamodb-table.sh
@@ -33,6 +33,7 @@ for c in `seq 0 0`; do
             echo $i
             if [[ "$1" != "" ]]; then
                 echo "1"
+                echo $awsout | jq -r "."
                 cname=`echo $awsout | jq -r ".${pref[(${c})]}[(${i})].${idfilt[(${c})]}"`
                 echo $awsout | jq -r ".${pref[(${c})]}[(${i})]"
             else

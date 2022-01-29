7c19b4ddaafd690e1c46fae67b38a8b76d1fac1d
awsandy <awsandy@amazon.co.uk>
2022-01-29 19:20:07 +0000

    up

--- scripts/parallel_import2.sh
+++ scripts/parallel_import2.sh
@@ -52,25 +52,25 @@ if [[ $? -ne 0 ]];then
     pwd
     ls -l  
     sl=`echo $((1 + $RANDOM % 15))`         
-    nice -n $sl terraform -state $st import $ttft.$rname "$cname" > /dev/null
+    nice -n $sl terraform import -state $st $ttft.$rname "$cname" > /dev/null
     if [ $? -ne 0 ]; then
         echo "Import backoff & retry for $rname"
         sl=`echo $((1 + $RANDOM % 10))`
         sleep $sl
-        nice -n $sl terraform  -state $st import $ttft.$rname "$cname" > /dev/null
+        nice -n $sl terraform import -state $st $ttft.$rname "$cname" > /dev/null
         if [ $? -ne 0 ]; then
                 echo "Import long backoff & retry with full errors for $rname"
                 sl=`echo $((2 + $RANDOM % 20))`
                 sleep $sl
-                nice -n $sl terraform  -state $st import $ttft.$rname "$cname" > /dev/null
+                nice -n $sl terraform import -state $st $ttft.$rname "$cname" > /dev/null
         fi
     fi
     #echo "local state list"
     #terraform state list -no-color
 
-    printf "terraform  -state $st import %s.%s %s" $ttft $rname "$cname" > ../data/import_$ttft_$rname.sh
+    printf "terraform  import -state $st %s.%s %s" $ttft $rname "$cname" > ../data/import_$ttft_$rname.sh
 
-    terraform  -state $st state show $ttft.$rname | perl -pe 's/\x1b.*?[mGKH]//g' > $ttft-$rname-1.txt 
+    terraform  state show -state $st $ttft.$rname | perl -pe 's/\x1b.*?[mGKH]//g' > $ttft-$rname-1.txt 
     rm $ttft.$rname.tf
 
 else

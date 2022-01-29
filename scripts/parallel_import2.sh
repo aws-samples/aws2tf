e0c3db82d437fb5e96a07b0c60dfc6887a10d211
awsandy <awsandy@amazon.co.uk>
2022-01-29 20:49:33 +0000

    up

--- scripts/parallel_import2.sh
+++ scripts/parallel_import2.sh
@@ -10,7 +10,7 @@ rname=${cname//:/_} && rname=${rname//./_} && rname=${rname//\//_}
 st=`printf "%s__%s.tfstate" $1 $rname`
 if [ -f "$st" ] ; then echo "$st exists already skipping" && exit; fi
 
-echo "parallel2 list check"
+#echo "parallel2 list check"
 (terraform state list 2> /dev/null | grep ${ttft}.${rname}) > /dev/null 
 if [[ $? -ne 0 ]];then
 
@@ -22,7 +22,7 @@ if [[ $? -ne 0 ]];then
     #cp ../aws.tf .
     ls ../.terraform > /dev/null
     if [[ $? -eq 0 ]];then 
-        echo "pi2 using root provider"
+        #echo "pi2 using root provider"
         ln -s ../aws.tf aws.tf  2> /dev/null
         ln -s ../.terraform .terraform 2> /dev/null
         ln -s ../.terraform.lock.hcl .terraform.lock.hcl 2> /dev/null
@@ -73,7 +73,7 @@ if [[ $? -ne 0 ]];then
     #terraform state show -state $st $ttft.$rname
 
     terraform state show -state $st $ttft.$rname | perl -pe 's/\x1b.*?[mGKH]//g' > ../$ttft-$rname-1.txt 
-    rm -r $fn
+    rm -f $fn
 
 else
     echo "State $ttft.$rname already exists skipping import ..."

afd48127d57ffd50082cc966948a1dcacba50ad2
awsandy <awsandy@amazon.co.uk>
2022-01-30 12:35:36 +0000

    up

--- scripts/056-get-instance-profile.sh
+++ scripts/056-get-instance-profile.sh
@@ -45,12 +45,13 @@ for c in `seq 0 0`; do
             fn=`printf "%s__%s.tf" $ttft $rname`
             if [ -f "$fn" ] ; then echo "$fn exists already skipping" && continue; fi
             #echo "calling import sub"
-            . ../../scripts/parallel_import.sh $ttft $cname &
+            . ../../scripts/parallel_import2.sh $ttft $cname &
         done
         jc=`jobs -r | wc -l | tr -d ' '`
         echo "Waiting for $jc Terraform imports"
         wait
         echo "Finished importing"
+        ../../scripts/parallel_statemv.sh $ttft
 
         for i in `seq 0 $count`; do
             #echo $i

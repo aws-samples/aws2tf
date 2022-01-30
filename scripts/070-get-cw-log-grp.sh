424fb8ae19974e20ed06d45b3f66045a5c6f3503
awsandy <awsandy@amazon.co.uk>
2022-01-30 09:38:03 +0000

    up

--- scripts/070-get-cw-log-grp.sh
+++ scripts/070-get-cw-log-grp.sh
@@ -25,7 +25,7 @@ for c in `seq 0 0`; do
     if [ "$count" -gt "0" ]; then
         count=`expr $count - 1`
         for i in `seq 0 $count`; do
-            #echo $i
+            echo $i
             cname=$(echo $awsout | jq -r ".${pref[(${c})]}[(${i})].${idfilt[(${c})]}")
             rname=${cname//:/_} && rname=${rname//./_} && rname=${rname//\//_}
             echo "$ttft $cname import"

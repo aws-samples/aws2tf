45550e8a3985d1b81aa645f09875bc87838733fc
awsandy <awsandy@amazon.co.uk>
2022-04-28 19:49:06 +0100

    up

--- scripts/get-glue-table.sh
+++ scripts/get-glue-table.sh
@@ -86,7 +86,8 @@ for c in `seq 0 0`; do
                     if [[ ${tt1} == "owner_id" ]];then skip=1;fi
                     # these are difficult to process so skip for now
                     if [[ ${tt1} == *"grokPattern"* ]];then 
-                        #tt2=`echo "$tt2" | tr -d '"'`
+                        tt2=${tt2%\"}
+                        tt2=${tt2#\"}
                         tt2=${tt2//\\/\\\\}
                         tt2=${tt2//%\{/%%\{}
                         t1=`printf "\"grokPattern\" = \"%s\"" "$tt2"`

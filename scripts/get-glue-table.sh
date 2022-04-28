2ea25862579154839df8337e2630136e5bbf75cd
awsandy <awsandy@amazon.co.uk>
2022-04-28 18:54:17 +0100

    up

--- scripts/get-glue-table.sh
+++ scripts/get-glue-table.sh
@@ -89,7 +89,7 @@ for c in `seq 0 0`; do
                         tt2=`echo "$tt2" | tr -d '"'`
                         tt2=${tt2//\\/\\\\}
                         tt2=${tt2//%{/%%{}}
-                        t1=`printf "type = \"%s\"" $tt2`
+                        t1=`printf "\"grokPattern\" = \"%s\"" $tt2`
                     fi
                     
                     if [[ ${tt1} == *"input.format"* ]];then skip=1;fi
@@ -105,7 +105,7 @@ for c in `seq 0 0`; do
                         #printf "    default = \"%s\" \n" $tt2 >> $gn
                         #printf "}\n" >> $gn
                         #t1=`printf "type = var.g-%s" $r1`
-                        t1=`printf "type = \"%s\"" $tt2`
+                        t1=`printf "type = \"%s\"/n" $tt2`
                         fi
                     fi
                 fi

eaae51c1df59b7f64d47503c44c49d30b883887e
awsandy <awsandy@amazon.co.uk>
2022-04-28 21:09:19 +0100

    up

--- scripts/get-glue-table.sh
+++ scripts/get-glue-table.sh
@@ -88,11 +88,12 @@ for c in `seq 0 0`; do
                     if [[ ${tt1} == *"grokPattern"* ]];then 
                         tt2=${tt2%\"}
                         tt2=${tt2#\"}
-                        echo tt2=$tt2
+                        echo "tt2 pq=$tt2"
                         tt2=${tt2//\\/\\\\}
                         tt2=${tt2//%\{/%%\{}
                         tt2=${tt2//"/\\"}
-                        echo tt2=$tt2
+                        echo tt2 q=$tt2
+                        skip=1
                         t1=`printf "\"grokPattern\" = %s\"" "$tt2"`
                     fi
                     

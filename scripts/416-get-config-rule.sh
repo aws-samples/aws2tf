bb46c0853bacb96b2444cf3a5fe4d332bf584292
awsandy <awsandy@amazon.co.uk>
2022-04-29 16:34:50 +0100

    up

--- scripts/416-get-config-rule.sh
+++ scripts/416-get-config-rule.sh
@@ -73,7 +73,7 @@ for c in `seq 0 0`; do
                         #echo $dl $tt2
                         if [[ $dl -gt 254 ]];then 
                         tt2=${tt2:0:252}; 
-                        #echo "shortened"
+                        echo "--> description shortened"
                         fi
                         dl=${#tt2}
                         #echo $dl $tt2

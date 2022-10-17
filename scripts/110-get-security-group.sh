9c95e3eb2fc5fbdec80030e688472e991bee3df3
awsandy <awsandy@amazon.co.uk>
2022-10-17 19:53:17 +0100

    Throttling message

--- scripts/110-get-security-group.sh
+++ scripts/110-get-security-group.sh
@@ -61,7 +61,7 @@ for c in `seq 0 0`; do
             . ../../scripts/parallel_import2.sh $ttft $cname &
             jc=`jobs -r | wc -l | tr -d ' '`
             while [ $jc -gt 15 ];do
-                echo "pausing $jc Terraform imports in progress"
+                echo "Throttling - $jc Terraform imports in progress"
                 sleep 10
                 jc=`jobs -r | wc -l | tr -d ' '`
             done
--- scripts/get-security-group.sh
+++ scripts/get-security-group.sh
@@ -37,12 +37,12 @@ for c in `seq 0 0`; do
             . ../../scripts/parallel_import2.sh $ttft $cname &
             jc=`jobs -r | wc -l | tr -d ' '`
             while [ $jc -gt 15 ];do
-                echo "pausing $jc Terraform imports in progress"
+                echo "Throttling - $jc Terraform imports in progress"
                 sleep 10
                 jc=`jobs -r | wc -l | tr -d ' '`
             done
         done
-        
+
         jc=`jobs -r | wc -l | tr -d ' '`
         echo "Waiting for $jc Terraform imports"
         wait

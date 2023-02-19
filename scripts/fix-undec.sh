ca300501ae9e3201eb1605c2574126749798ac69
awsandy <awsandy@amazon.co.uk>
2023-02-19 09:56:48 +0000

    up

--- scripts/fix-undec.sh
+++ scripts/fix-undec.sh
@@ -28,7 +28,7 @@ for c in $(seq 0 $count); do
                 eval $cmd
             fi
         fi
-        if [[ $tft == "aws_vpc" ]] || [[ $tft == "aws_subnet" ]]; then
+        if [[ $tft == "aws_vpc" ]] || [[ $tft == "aws_subnet" ]] || [[ $tft == "aws_ec2_transit_gateway_vpc_attachment" ]]; then
                 cmd=$(printf "sed -i'.orig' -e 's/%s/\"%s\"/g' ${fil}" $res $addr)
                 echo "Undeclared Fix --> $res"
                 eval $cmd

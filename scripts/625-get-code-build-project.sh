ce23002011d1553432820343ab4358dc1bdb1525
awsandy <awsandy@amazon.co.uk>
2023-06-29 18:32:27 +0100

    up

--- scripts/625-get-code-build-project.sh
+++ scripts/625-get-code-build-project.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mysub=$(echo $AWS2TF_ACCOUNT)
+myreg=$(echo $AWS2TF_REGION)
 if [ "$1" != "" ]; then
     cmd[0]="$AWS codebuild list-projects | jq -r '.projects[] | select(.==\"${1}\")'"
 else

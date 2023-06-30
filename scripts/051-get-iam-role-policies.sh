95865cc40e3cbb1fc5c849b62a57f9e6c87d812d
awsandy <awsandy@amazon.co.uk>
2023-06-30 14:47:24 +0100

    up

--- scripts/051-get-iam-role-policies.sh
+++ scripts/051-get-iam-role-policies.sh
@@ -166,7 +166,7 @@ for c in $(seq 0 0); do
                                 tacc=$(echo $tt2 | cut -f5 -d ':')
                                 tend=$(echo $tt2 | cut -f6- -d ':')
                                 tsub="%s"
-                                if [[ "$treg" != "" ]] !! [[ "$tacc" != "" ]]; then
+                                if [[ "$treg" != "" ]] || [[ "$tacc" != "" ]]; then
                                     if [[ "$mysub" == "$tacc" ]]; then
                                         t1=$(printf "%s = format(\"%s:%s:%s:%s*\",data.aws_region.current.name,data.aws_caller_identity.current.account_id)" $tt1 $tstart $tsub $tsub $tend)
                                     fi

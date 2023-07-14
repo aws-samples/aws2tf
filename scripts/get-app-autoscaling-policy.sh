c97bcac85676aa782a11de9f162e6583a52cd3c2
awsandy <awsandy@amazon.co.uk>
2023-07-14 16:06:41 +0100

    up

--- scripts/get-app-autoscaling-policy.sh
+++ scripts/get-app-autoscaling-policy.sh
@@ -93,7 +93,8 @@ for i in $(seq 0 $count); do
             if [[ ${tt1} == "owner_id" ]]; then skip=1; fi
 
             if [[ ${tt1} == "resource_id" ]]; then
-                t1=$(printf "%s = service/${aws_ecs_cluster.%s.name}/${aws_ecs_service.%s.name}" $tt1 $2 $3)
+                percs="%s"
+                t1=$(printf "%s = format(\"service/%s/%s\",aws_ecs_cluster.%s.name,aws_ecs_service.%s.name)" $tt1 $percs $percs $2 $3)
             fi
 
         fi

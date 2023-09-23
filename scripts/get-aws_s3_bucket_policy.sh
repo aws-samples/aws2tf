ab05d676bfcaa0fe250c5b35ae331b31da65c7e9
awsandy <awsandy@amazon.co.uk>
2023-09-23 17:42:44 +0100

    up

--- scripts/get-aws_s3_bucket_policy.sh
+++ scripts/get-aws_s3_bucket_policy.sh
@@ -61,7 +61,7 @@ while IFS= read t1; do
         if [[ ${tt1} == *":"* ]]; then
             tt1=$(echo $tt1 | tr -d '"')
             tt2=$(echo $tt2 | tr -d '"')
-            echo "--> $tt1 $tt2 $mysub"
+            #echo "--> $tt1 $tt2 $mysub"
             if [[ "$tt2" != "$mysub" ]]; then
                 #if [[ ${tt2} == *":"* ]]; then
                 t1=$(printf "\"%s\"=\"%s\"" $tt1 $tt2)

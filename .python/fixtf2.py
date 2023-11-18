1d7aa0e2c0945a030f66a50dd6ee4ff7062cf216
awsandy <awsandy@amazon.co.uk>
2023-11-18 09:03:42 +0000

    up

--- .python/fixtf2.py
+++ .python/fixtf2.py
@@ -294,7 +294,11 @@ def aws_vpclattice_service_network_vpc_association(t1,tt1,tt2,flag1,flag2):
 def aws_eks_cluster(t1,tt1,tt2,flag1,flag2):
     skip=0
     if tt1 == "subnet_ids":  t1,skip = deref_array(t1,tt1,tt2,"aws_subnet","subnet-",skip)
-    if tt1 == "security_group_ids": t1,skip = deref_array(t1,tt1,tt2,"aws_security_group","sg-",skip)
+    elif tt1 == "security_group_ids": t1,skip = deref_array(t1,tt1,tt2,"aws_security_group","sg-",skip)
+    elif tt1 == "role_name":
+        tt2=tt2.strip('\"')
+        t1=tt1 + " = aws_iam_role." + tt2 + ".id\n"
+        add_dendendancy("aws_iam_role",tt2)
     return skip,t1,flag1,flag2
 
 

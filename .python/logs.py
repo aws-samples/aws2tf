b06d143885e91e670114c8468da900307b9dfa6e
awsandy <awsandy@amazon.co.uk>
2023-12-17 17:50:17 +0000

    up

--- .python/logs.py
+++ .python/logs.py
@@ -1,24 +1,34 @@
 import common
 import globals
 
+
 def get_aws_cloudwatch_log_group(type, id, clfn, descfn, topkey, key, filterid):
     if globals.debug:
         print("--> In get_aws_cloudwatch_log_group  doing " + type + ' with id ' + str(id) +
               " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
-    
+
     response = common.call_boto3(clfn, descfn, topkey, id)
     print("-9a->"+str(response))
     if response == []:
         print("empty response returning")
         return
-    for j in response:
+    if id is not None:
         if "arn:" in id:
-            tarn=j['arn']
-            if tarn==id:
-                retid = j[key]
-                theid = retid
-                common.write_import(type, theid, id)    
+            for j in response:
+                tarn = j['arn']
+                if tarn == id:
+                    retid = j[key]
+                    theid = retid
+                    common.write_import(type, theid, id)
         else:
+            for j in response:
+                tnam = j['logGroupName']
+                if tnam == id:
+                    retid = j[key]
+                    theid = retid
+                    common.write_import(type, theid, id)
+    else:
+        for j in response:
             retid = j[key]
             theid = retid
             common.write_import(type, theid, id)

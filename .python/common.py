703918c59fc2cbf6482372e91311cf8075122339
awsandy <awsandy@amazon.co.uk>
2023-12-17 17:33:00 +0000

    up

--- .python/common.py
+++ .python/common.py
@@ -58,7 +58,6 @@ def call_resource(type, id):
    except Exception as e:      
                 # By this way we can know about the type of error occurring
                 print(f"{e=}")
-                
                 exc_type, exc_obj, exc_tb = sys.exc_info()
                 fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
                 print(exc_type, fname, exc_tb.tb_lineno)    

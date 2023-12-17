9eb61108761c881bfa9bc29fae7f0d1973627f79
awsandy <awsandy@amazon.co.uk>
2023-12-17 17:45:40 +0000

    up

--- .python/logs.py
+++ .python/logs.py
@@ -5,8 +5,9 @@ def get_aws_cloudwatch_log_group(type, id, clfn, descfn, topkey, key, filterid):
     if globals.debug:
         print("--> In get_aws_cloudwatch_log_group  doing " + type + ' with id ' + str(id) +
               " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
+    
     response = common.call_boto3(clfn, descfn, topkey, id)
-    # print("-9a->"+str(response))
+    print("-9a->"+str(response))
     if response == []:
         print("empty response returning")
         return

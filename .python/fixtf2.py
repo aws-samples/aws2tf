44491ea599db569b9580597aa4f2e1d2b60a0395
awsandy <awsandy@amazon.co.uk>
2024-01-02 13:42:00 +0000

    up

--- .python/fixtf2.py
+++ .python/fixtf2.py
@@ -667,6 +667,13 @@ def aws_lambda_event_source_mapping(t1,tt1,tt2,flag1,flag2):
 
 #######################
 
+def aws_redshiftserverless_namespace(t1,tt1,tt2,flag1,flag2):
+    skip=0
+    return skip,t1,flag1,flag2 
+
+def aws_redshiftserverless_workgroup(t1,tt1,tt2,flag1,flag2):
+    skip=0
+    return skip,t1,flag1,flag2 
 
 
 

3531fef682d58efecf6a7a1c18e04414a93c335e
awsandy <awsandy@amazon.co.uk>
2024-01-10 18:26:13 +0000

    up

--- .python/.auto-gen/prstack.py
+++ .python/.auto-gen/prstack.py
@@ -1,69 +1,23 @@
 #!/usr/bin/env python3
 
-st1 = False
-fr = open('../resources.py', 'r')
-line = fr.readline()
-while line:
-    line = fr.readline()
-    myline = str(line).strip()
-    # if "START AUTOGEN" in myline: st1=True
-    # if st1:
-    if '#' in myline:
-        continue
-    if 'aws_' in myline and 'elif' in myline and '[' not in myline:
-        awsr = myline.split('"')[1]
-        line = fr.readline()
-        myline2 = str(line).strip()
-        clfn = myline2.split(';')[0].split('=')[1]
-        descfn = myline2.split(';')[1].split('=')[1].strip('\"')
-        topkey = myline2.split(';')[2].split('=')[1].strip('\"')
-        key = myline2.split(';')[3].split('=')[1].strip('\"')
-        filterid = myline2.split(';')[4].split('=')[1].strip('\"')
-        try:
-            clfn = clfn.split('"')[1]
-        except:
-            print("indexerror on clfn="+clfn)
-            print(myline)
-            print(myline2)
-            exit()
-        # print(awsr+": cl="+clfn+": des="+descfn+": tk="+topkey+": ky="+key+"; fil="+filterid)
-        if filterid == 'key':
-            filterid = key
-        print(awsr+": cl="+clfn+": des="+descfn+": tk=" +
-              topkey+": ky="+key+"; fil="+filterid)
-        of = open('aws_dict.py', 'a')
-        of.write(awsr + ' = {\n')
-        of.write('\t"clfn":\t\t"' + clfn + '",\n')
-        of.write('\t"descfn":\t"' + descfn + '",\n')
-        of.write('\t"topkey":\t"' + topkey + '",\n')
-        of.write('\t"key":\t\t"' + key + '",\n')
-        of.write('\t"filterid":\t"' + filterid + '"\n')
-        of.write('}\n\n')
 
 
-of.close()
+
 
 # end part 1
 
 st1 = False
-fr = open('../resources.py', 'r')
+fr = open('final-cf-resources.dat', 'r')
+of = open('cf-stack.py', 'w')
 line = fr.readline()
-of = open('aws_dict.py', 'a')
-of.write('aws_resources = {\n')
 while line:
     line = fr.readline()
     myline = str(line).strip()
     # if "START AUTOGEN" in myline: st1=True
-    # if st1:
-    if '#' in myline:
-        continue
-    if 'aws_' in myline and 'elif' in myline and '[' not in myline:
-        awsr = myline.split('"')[1]
-        print(awsr)
-        of.write('\t"'+awsr+'": ' + awsr + ',\n')
+    of.write('\t\t\telif type == "'+myline+'": common.call_resource("aws_null", type+" "+pid)\n')
 
 
-of.write('}\n\n')
+fr.close()
 of.close()
 
 

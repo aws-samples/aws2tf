1fe5ed6d6184acacd6d7e5f9d729bd43c6807bf8
awsandy <awsandy@amazon.co.uk>
2024-03-12 12:21:23 +0000

    up

--- .python/fixtf_aws_resources/fixtf_glue.py
+++ .python/fixtf_aws_resources/fixtf_glue.py
@@ -73,6 +73,9 @@ def aws_glue_security_configuration(t1,tt1,tt2,flag1,flag2):
 
 def aws_glue_trigger(t1,tt1,tt2,flag1,flag2):
 	skip=0
+	if tt1 == "timeout": 
+		if tt2 == "0": skip=1
+	
 	return skip,t1,flag1,flag2
 
 def aws_glue_user_defined_function(t1,tt1,tt2,flag1,flag2):
--- .python/get_aws_resources/aws_glue.py
+++ .python/get_aws_resources/aws_glue.py
@@ -22,6 +22,7 @@ def get_aws_glue_catalog_database(type, id, clfn, descfn, topkey, key, filterid)
                 pkey=globals.acc+":"+j[key]
                 tfid="d-"+pkey.replace(":","__")
                 common.write_import(type,pkey,tfid) 
+                common.add_dependancy("aws_glue_catalog_table",pkey)
 
         else:          
             response = client.get_database(Name=id)
@@ -30,6 +31,7 @@ def get_aws_glue_catalog_database(type, id, clfn, descfn, topkey, key, filterid)
             pkey=globals.acc+":"+j[key]
             tfid="d-"+pkey.replace(":","__")
             common.write_import(type,pkey,tfid)
+            print("Add dep aws_glue_catalog_table "+pkey)
             common.add_dependancy("aws_glue_catalog_table",pkey)
 
     except Exception as e:
@@ -56,12 +58,7 @@ def get_aws_glue_catalog_table(type, id, clfn, descfn, topkey, key, filterid):
     try:
         response = []
         client = boto3.client(clfn)
-        cc=id.count('/')
-        if cc == 1:
-             catalogn=id.split('/')[0]
-             databasen=id.split('/')[1]
-        if cc == 2:
-             tabnam=id.split('/')[2]
+
 
         
         if id is None:
@@ -69,7 +66,12 @@ def get_aws_glue_catalog_table(type, id, clfn, descfn, topkey, key, filterid):
  
         else:     
             ## Do not have table name
-
+            cc=id.count(':')
+            if cc == 1:
+                catalogn=id.split(':')[0]
+                databasen=id.split(':')[1]
+            if cc == 2:
+                tabnam=id.split(':')[2]
 
             ## Do have table name
             if cc == 1:
@@ -83,10 +85,50 @@ def get_aws_glue_catalog_table(type, id, clfn, descfn, topkey, key, filterid):
                 pkey=catalogn+":"+databasen+":"+j[key]
                 tfid="d-"+pkey.replace(":","__")
                 common.write_import(type,pkey,tfid)
+            
+            # set dependency false
+            tkey="aws_glue_catalog_table"+"."+catalogn+":"+databasen
+            print("Setting True "+tkey)
+            globals.rproc[tkey]=True
 
     except Exception as e:
             print(f"{e=}")
             print("ERROR: -2->unexpected error in get_aws_glue_catalog_database")
+            print("clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" id="+str(id))
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
+            exit()
+
+    return True
+
+
+def get_aws_glue_trigger(type, id, clfn, descfn, topkey, key, filterid):
+
+    if globals.debug:
+        print("--> In get_aws_glue_trigger  doing " + type + ' with id ' + str(id) +
+              " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
+        
+    try:
+        response = []
+        client = boto3.client(clfn)
+        if id is None:
+            response = client.list_triggers()
+            if response == []: print("Empty response for "+type+ " id="+str(id)+" returning"); return True
+            for j in response['TriggerNames']:
+                pkey=j
+                common.write_import(type,pkey,None) 
+
+        else:          
+            response = client.get_trigger(Name=id)
+            if response == []: print("Empty response for "+type+ " id="+str(id)+" returning"); return True
+            j=response['Trigger']
+            pkey=j[key]
+            common.write_import(type,pkey,None)
+
+    except Exception as e:
+            print(f"{e=}")
+            print("ERROR: -2->unexpected error in get_aws_glue_trigger")
             print("clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" id="+str(id))
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]

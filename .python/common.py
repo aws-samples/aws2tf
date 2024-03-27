ca0c7a900b64b1e8391ffa916b58771513003fef
awsandy <awsandy@amazon.co.uk>
2024-03-27 12:48:13 +0000

    up

--- .python/common.py
+++ .python/common.py
@@ -34,6 +34,9 @@ from fixtf_aws_resources import aws_no_import
 
 def call_resource(type, id):
     
+   if aws_no_import.noimport[type]:
+      print("WARNING: Terraform cannot import type: " + type)
+      return
    if type=="aws_null":
       with open('stack-null.log', 'a') as f3:
          f3.write("-->> called aws_null for: "+id+"\n")

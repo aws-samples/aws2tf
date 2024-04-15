c8c1644de6dc9967822d9b7f1b287f000bc7d1fe
awsandy <awsandy@amazon.co.uk>
2024-04-15 21:27:42 +0100

    up

--- .python/fixtf_aws_resources/fixtf_redshift.py
+++ .python/fixtf_aws_resources/fixtf_redshift.py
@@ -2,20 +2,24 @@ import common
 import fixtf
 
 def aws_redshift_cluster(t1,tt1,tt2,flag1,flag2):
-    skip=0
+	skip=0
     ##if tt1 == "vpc_security_group_ids": t1,skip = fixtf.deref_array(t1,tt1,tt2,"aws_security_group","sg-",skip)
     ## if tt1 == "iam_roles":    t1=fixtf.deref_role_arn_array(t1,tt1,tt2)
-    if tt1 == "cluster_subnet_group_name":
+	if tt1 == "cluster_subnet_group_name":
         ##tt2=tt2.strip('\"')
-        t1=tt1 + " = aws_redshift_subnet_group." + tt2 + ".id\n"
-        common.add_dependancy("aws_redshift_subnet_group",tt2)
-    elif tt1 == "cluster_parameter_group_name":
-        ##tt2=tt2.strip('\"')
-        t1=tt1 + " = aws_redshift_parameter_group." + tt2 + ".id\n"
-        common.add_dependancy("aws_redshift_parameter_group",tt2)
+		t1=tt1 + " = aws_redshift_subnet_group." + tt2 + ".id\n"
+		common.add_dependancy("aws_redshift_subnet_group",tt2)
+	elif tt1 == "cluster_parameter_group_name" and tt2 != "null":
+		if not tt2.startswith("default"):
+			t1=tt1 + " = aws_redshift_parameter_group." + tt2 + ".id\n"
+			common.add_dependancy("aws_redshift_parameter_group",tt2)
     #elif tt1 == "kms_key_id":    t1=fixtf.deref_kms_key(t1,tt1,tt2)
+	elif tt1 == "apply_immediately":
+		if tt2=="null":
+			t1=tt1+" = false \n lifecycle {\n   ignore_changes = [apply_immediately,cluster_version]\n}\n"
+        ##tt2=tt2.strip('\"')
     
-    return skip,t1,flag1,flag2 
+	return skip,t1,flag1,flag2 
 
 def aws_redshift_subnet_group(t1,tt1,tt2,flag1,flag2):
     skip=0

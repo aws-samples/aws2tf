d6a3a9811741db79727add7356540a96dc349387
Andy Thomas <awsandy@amazon.co.uk>
2024-05-26 09:52:14 +0100

    up

--- .python/fixtf_aws_resources/fixtf_neptune.py
+++ .python/fixtf_aws_resources/fixtf_neptune.py
@@ -1,5 +1,10 @@
 def aws_neptune_cluster(t1,tt1,tt2,flag1,flag2):
 	skip=0
+	if tt1=="engine":
+		t1 = tt1 + " = \"neptune\"\n"
+		t1 = t1  + "\n lifecycle {\n   ignore_changes = [engine,serverless_v2_scaling_configuration[0].min_capacity]\n}\n"
+	if tt1=="min_capacity" and tt2=="0.5": 
+		t1 = tt1 + " = \"1.0\"\n"
 	return skip,t1,flag1,flag2
 
 def aws_neptune_cluster_endpoint(t1,tt1,tt2,flag1,flag2):

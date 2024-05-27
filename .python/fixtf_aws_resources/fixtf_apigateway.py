a458c43737921413cb1653166a680a0dd851672e
Andy Thomas <awsandy@amazon.co.uk>
2024-05-27 12:50:22 +0100

    up

--- .python/fixtf_aws_resources/fixtf_apigateway.py
+++ .python/fixtf_aws_resources/fixtf_apigateway.py
@@ -5,8 +5,6 @@ import globals
 
 def aws_api_gateway_account(t1,tt1,tt2,flag1,flag2):
 	skip=0
-	if tt1 == "cloudwatch_role_arn": 
-		t1=fixtf.globals_replace(t1,tt1,tt2)
 	return skip,t1,flag1,flag2
 
 def aws_api_gateway_api_key(t1,tt1,tt2,flag1,flag2):

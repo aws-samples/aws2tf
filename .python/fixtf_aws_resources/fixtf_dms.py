7ecd6b8e5617010da15a6d0c2b252eff8989286d
Andy Thomas <awsandy@amazon.co.uk>
2024-07-10 14:58:29 +0100

    up

--- .python/fixtf_aws_resources/fixtf_dms.py
+++ .python/fixtf_aws_resources/fixtf_dms.py
@@ -29,6 +29,9 @@ def aws_dms_replication_subnet_group(t1,tt1,tt2,flag1,flag2):
 
 def aws_dms_replication_task(t1,tt1,tt2,flag1,flag2):
 	skip=0
+	if tt1=="CloudWatchLogGroup": skip=1
+	if tt1=="CloudWatchLogStream": skip=1
+
 	return skip,t1,flag1,flag2
 
 def aws_dms_s3_endpoint(t1,tt1,tt2,flag1,flag2):

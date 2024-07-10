b28f90c34960d67e660f75e54dc72d9c1e897794
Andy Thomas <awsandy@amazon.co.uk>
2024-07-10 14:20:40 +0100

    up

--- .python/fixtf_aws_resources/fixtf_dms.py
+++ .python/fixtf_aws_resources/fixtf_dms.py
@@ -1,3 +1,5 @@
+import common
+
 def aws_dms_certificate(t1,tt1,tt2,flag1,flag2):
 	skip=0
 	return skip,t1,flag1,flag2
@@ -18,6 +20,7 @@ def aws_dms_replication_instance(t1,tt1,tt2,flag1,flag2):
 	skip=0
 	if tt1=="replication_subnet_group_id" and tt2 != "null":
 		t1=tt1 + " = aws_dms_replication_subnet_group." + tt2 + ".id\n"
+		common.add_dependancy("aws_dms_replication_subnet_group",tt2)
 	return skip,t1,flag1,flag2
 
 def aws_dms_replication_subnet_group(t1,tt1,tt2,flag1,flag2):

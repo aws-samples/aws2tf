991c0bccce405046dba2e0e57935ddb146701132
Andy Thomas <awsandy@amazon.co.uk>
2024-07-16 21:59:46 +0100

    sso stuff

--- .python/get_aws_resources/aws_sso_admin.py
+++ .python/get_aws_resources/aws_sso_admin.py
@@ -46,6 +46,83 @@ def get_aws_ssoadmin_permission_set(type, id, clfn, descfn, topkey, key, filteri
             pkey=j+","+id
             theid=pkey.replace(",", "_")
             common.write_import(type,pkey,theid)
+            common.add_dependancy("aws_ssoadmin_managed_policy_attachment", pkey)
+            common.add_dependancy("aws_ssoadmin_permission_set_inline_policy", pkey)
+
+    except Exception as e:
+        common.handle_error(e,str(inspect.currentframe().f_code.co_name),clfn,descfn,topkey,id)
+
+    return True
+
+
+def get_aws_ssoadmin_managed_policy_attachment(type, id, clfn, descfn, topkey, key, filterid):
+    if globals.debug:
+        print("--> In "+str(inspect.currentframe().f_code.co_name)+" doing " + type + ' with id ' + str(id) +
+              " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
+    try:
+        # id - instid arm /perm arn
+        if "," not in id: print("No SSO instance found"); return True
+
+        response = []
+        client = boto3.client(clfn)  
+        inid=id.split(",")[1]; psarn=id.split(",")[0] 
+        print("id="+id)
+        response = client.list_managed_policies_in_permission_set(InstanceArn=inid,PermissionSetArn=psarn)
+        if response == []: 
+            print("Empty response for "+type+ " id="+str(id)+" returning")
+            rkey=type+"."+psarn+","+inid
+            globals.rproc[rkey] = True
+            return True
+        for j in response['AttachedManagedPolicies']:
+            mparn=j['Arn']
+            print("----->>>>>> mparn="+mparn)
+            pkey=mparn+","+psarn+","+inid
+            theid=pkey.replace(",", "_")
+            common.write_import(type,pkey,theid)
+        rkey=type+"."+psarn+","+inid
+        globals.rproc[rkey] = True
+
+    except Exception as e:
+        common.handle_error(e,str(inspect.currentframe().f_code.co_name),clfn,descfn,topkey,id)
+
+    return True
+
+
+def get_aws_ssoadmin_permission_set_inline_policy(type, id, clfn, descfn, topkey, key, filterid):
+    if globals.debug:
+        print("--> In "+str(inspect.currentframe().f_code.co_name)+" doing " + type + ' with id ' + str(id) +
+              " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
+    try:
+        # id - instid arm /perm arn
+        if "/" not in id: print("No SSO instance found"); return True
+
+        response = []
+        client = boto3.client(clfn)  
+        inid=id.split(",")[1]; psarn=id.split(",")[0]  
+        response = client.get_inline_policy_for_permission_set(InstanceArn=inid,PermissionSetArn=psarn)
+        if response == []: 
+            print("Empty response for "+type+ " id="+str(id)+" returning")
+            rkey=type+"."+psarn+","+inid
+            globals.rproc[rkey] = True
+            return True
+
+        j=response['InlinePolicy']
+        if j == {}: 
+            print("Empty inline policy [1] for "+type+ " id="+str(id)+" returning")
+            rkey=type+"."+psarn+","+inid
+            globals.rproc[rkey] = True
+            return True
+        if len(j) < 4: 
+            print("Empty inline policy [2] for "+type+ " id="+str(id)+" returning")
+            rkey=type+"."+psarn+","+inid
+            globals.rproc[rkey] = True
+            return True
+        pkey=psarn+","+inid
+
+        theid=pkey.replace(",", "_")
+        common.write_import(type,pkey,theid)
+        rkey=type+"."+psarn+","+inid
+        globals.rproc[rkey] = True
 
     except Exception as e:
         common.handle_error(e,str(inspect.currentframe().f_code.co_name),clfn,descfn,topkey,id)

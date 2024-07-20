320dd42e8738301215017b7af90893728f1b2366
awsandy <awsandy@amazon.co.uk>
2024-07-20 16:29:32 +0100

    cloudfront support

--- .python/get_aws_resources/aws_cloudfront.py
+++ .python/get_aws_resources/aws_cloudfront.py
@@ -34,8 +34,8 @@ def get_aws_cloudfront_distribution(type, id, clfn, descfn, topkey, key, filteri
     return True
 
 def get_aws_cloudfront_origin_access_identity(type, id, clfn, descfn, topkey, key, filterid):
-    #if globals.debug:
-    print("--> In "+str(inspect.currentframe().f_code.co_name)+" doing " + type + ' with id ' + str(id) +
+    if globals.debug:
+        print("--> In "+str(inspect.currentframe().f_code.co_name)+" doing " + type + ' with id ' + str(id) +
               " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
     try:
         response = []

f45539e791abb4daaa845c749c5018b263aa615f
Andy Thomas <awsandy@amazon.co.uk>
2024-07-21 15:23:55 +0100

    up

--- .python/get_aws_resources/aws_emr.py
+++ .python/get_aws_resources/aws_emr.py
@@ -107,6 +107,7 @@ def get_aws_emr_managed_scaling_policy(type, id, clfn, descfn, topkey, key, filt
 
         else:      
             response = client.get_managed_scaling_policy(ClusterId=id)
+            print(str(response))
             if response == []: 
                 print("Empty response for "+type+ " id="+str(id)+" returning"); 
                 pkey=type+"."+id

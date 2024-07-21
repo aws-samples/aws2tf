b5fb52e760a1b705f4bffe53848e254cc8cfcb5d
Andy Thomas <awsandy@amazon.co.uk>
2024-07-21 10:26:22 +0100

    up

--- .python/fixtf_aws_resources/fixtf_emr.py
+++ .python/fixtf_aws_resources/fixtf_emr.py
@@ -5,7 +5,7 @@ def aws_emr_block_public_access_configuration(t1,tt1,tt2,flag1,flag2):
 def aws_emr_cluster(t1,tt1,tt2,flag1,flag2):
 	skip=0
 	if tt1=="kdc_admin_password":
-		if tt2=="null":
+		if tt2.startswith("null"):
 			t1=tt1+" = CHANGE_ME\n"
 			t1=t1+"\n lifecycle {\n   ignore_changes = [kerberos_attributes[0].kdc_admin_password]\n}\n"
 	return skip,t1,flag1,flag2

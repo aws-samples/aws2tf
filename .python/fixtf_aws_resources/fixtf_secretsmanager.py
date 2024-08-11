dfd076f6ecaf8be4fbd635aac9500d4dda19b64c
Andy Thomas <awsandy@amazon.co.uk>
2024-08-11 07:41:21 +0100

    secretsmanager secrets

--- .python/fixtf_aws_resources/fixtf_secretsmanager.py
+++ .python/fixtf_aws_resources/fixtf_secretsmanager.py
@@ -1,3 +1,8 @@
+import common
+import boto3
+import globals
+import inspect
+
 def aws_secretsmanager_random_password(t1,tt1,tt2,flag1,flag2):
 	skip=0
 	return skip,t1,flag1,flag2
@@ -7,12 +12,34 @@ def aws_secretsmanager_secret_policy(t1,tt1,tt2,flag1,flag2):
 	return skip,t1,flag1,flag2
 
 def aws_secretsmanager_secret_rotation(t1,tt1,tt2,flag1,flag2):
-	skip=0
-	return skip,t1,flag1,flag2
+    skip=0
+    if tt1=="rotate_immediately" and tt2=="null":
+        t1=tt1+" = true\n" + " lifecycle {\n   ignore_changes = [rotate_immediately]\n}\n"
+    return skip,t1,flag1,flag2
 
 def aws_secretsmanager_secret_version(t1,tt1,tt2,flag1,flag2):
-	skip=0
-	return skip,t1,flag1,flag2
+    skip=0
+    ## need to get binary and string values
+    try:
+        if t1.startswith("resource"):
+            vid=t1.split("_")[-1]
+            vid=vid.replace("\"","").replace("{","").replace(" ","").replace("\n","")
+            globals.secvid=vid
+        elif tt1 == "secret_id":
+            globals.secid=tt2
+        elif tt1 == "secret_string":
+            if "null" in tt2:
+                client = boto3.client('secretsmanager')
+                response = client.get_secret_value(SecretId=globals.secid,VersionId=globals.secvid)
+                sv=response['SecretString']
+                t1 = tt1 + " = jsonencode("+sv+")\n"
+        if tt1 == "secret_binary": 
+            t1="\n lifecycle {\n   ignore_changes = [secret_binary,secret_string]\n}\n"
+
+    except Exception as e:
+        common.handle_error(e,str(inspect.currentframe().f_code.co_name),clfn,descfn,topkey,id)
+    
+    return skip,t1,flag1,flag2
 
 def aws_secretsmanager_secret(t1,tt1,tt2,flag1,flag2):
     skip=0
@@ -22,7 +49,6 @@ def aws_secretsmanager_secret(t1,tt1,tt2,flag1,flag2):
             t1 = tt1 + "= 30\n lifecycle {\n   ignore_changes = [recovery_window_in_days,force_overwrite_replica_secret]\n}\n"
 
     elif tt1 == "force_overwrite_replica_secret":
-        
         if tt2 == "null": 
             t1 = tt1 + "= false\n"
 
--- .python/get_aws_resources/aws_secretsmanager.py
+++ .python/get_aws_resources/aws_secretsmanager.py
@@ -1,8 +1,11 @@
 import common
+import fixtf
+import base64
 import boto3
+import sys
+import os
 import globals
 import inspect
-
 
 def get_aws_secretsmanager_secret(type, id, clfn, descfn, topkey, key, filterid):
     if globals.debug:
@@ -15,11 +18,18 @@ def get_aws_secretsmanager_secret(type, id, clfn, descfn, topkey, key, filterid)
         if id is None:
             # calls list_secrets
             paginator = client.get_paginator(descfn)
-            for page in paginator.paginate():
+            for page in paginator.paginate(IncludePlannedDeletion=False):
                 response = response + page[topkey]
             if response == []: print("Empty response for "+type+ " id="+str(id)+" returning"); return True
             for j in response:
+                
                 common.write_import(type,j[key],None) 
+                common.add_dependancy("aws_secretsmanager_secret_version",j[key])
+                try:
+                    print(j['RotationEnabled'])
+                    common.add_dependancy("aws_secretsmanager_secret_rotation",j[key])
+                except KeyError:
+                    print("INFO: No rotation config")
 
 
         else:
@@ -27,6 +37,72 @@ def get_aws_secretsmanager_secret(type, id, clfn, descfn, topkey, key, filterid)
             if response == []: print("Empty response for "+type+ " id="+str(id)+" returning"); return True
             j=response
             common.write_import(type,j[key],None)
+            common.add_dependancy("aws_secretsmanager_secret_rotation",j[key])
+            #common.add_dependancy("aws_secretsmanager_secret_version",j[key])
+
+
+    except Exception as e:
+        common.handle_error(e,str(inspect.currentframe().f_code.co_name),clfn,descfn,topkey,id)
+
+    return True
+
+
+
+def get_aws_secretsmanager_secret_rotation(type, id, clfn, descfn, topkey, key, filterid):
+    if globals.debug:
+        print("--> In get_aws_secretsmanager_secret_rotation  doing " + type + ' with id ' + str(id) +
+              " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
+        
+    try:
+        response = []
+        client = boto3.client(clfn)
+        if id is None:
+            print("ERROR: get_aws_secretsmanager_secret_rotation must be called with SecretID as parameter")
+        else:
+            common.write_import(type,id,None)
+         
+    except Exception as e:
+        common.handle_error(e,str(inspect.currentframe().f_code.co_name),clfn,descfn,topkey,id)
+
+    return True
+
+
+
+
+def get_aws_secretsmanager_secret_version(type, id, clfn, descfn, topkey, key, filterid):
+    if globals.debug:
+        print("--> In get_aws_secretsmanager_secret_version  doing " + type + ' with id ' + str(id) +
+              " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
+        
+    try:
+        response = []
+        client = boto3.client(clfn)
+        if id is None:
+            # calls list_secret_version
+            print("ERROR: get_aws_secretsmanager_secret_verion must be called with SecretID as parameter")
+        else:
+            response = client.list_secret_version_ids(SecretId=id,IncludeDeprecated=False)
+            if response == []: print("Empty response for "+type+ " id="+str(id)+" returning"); return True
+            #print(response)
+            for j in response[topkey]:
+                pkey=id+"|"+j[key]
+                common.write_import(type,pkey,None) 
+            pkey=type+"."+id
+            globals.rproc[pkey]=True
+
+    except Exception as e:
+        common.handle_error(e,str(inspect.currentframe().f_code.co_name),clfn,descfn,topkey,id)
+
+    return True
+
+def get_aws_secretsmanager_secret_policy(type, id, clfn, descfn, topkey, key, filterid):
+    if globals.debug:
+        print("--> In get_aws_secretsmanager_secret_policy  doing " + type + ' with id ' + str(id) +
+              " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
+        
+    try:
+        response = []
+        print("INFO: aws_secretsmanager_policy - policy embedded in aws_secretmager_secret by aws2tf")
 
 
     except Exception as e:

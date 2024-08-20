a909f2ccd3bcfa850bbb63d24227fbeee5e6dc37
Andy Thomas <awsandy@amazon.co.uk>
2024-08-20 11:48:09 +0100

    datazone support

--- .python/get_aws_resources/aws_datazone.py
+++ .python/get_aws_resources/aws_datazone.py
@@ -19,7 +19,9 @@ def get_aws_datazone_domain(type, id, clfn, descfn, topkey, key, filterid):
                 common.write_import(type,j[key],None) 
                 # provider crashes on import
                 common.add_dependancy("aws_datazone_project", j[key])
-                #common.add_dependancy("aws_datazone_environment_profile", j[key])
+                common.add_dependancy("aws_datazone_glossary", j[key])
+                common.add_dependancy("aws_datazone_glossary_term", j[key])
+                common.add_dependancy("aws_datazone_environment_profile", j[key])
 
         else:      
             response = client.get_domain(identifier=id)
@@ -28,7 +30,9 @@ def get_aws_datazone_domain(type, id, clfn, descfn, topkey, key, filterid):
             common.write_import(type,j[key],None)
             # provider crashes on import
             common.add_dependancy("aws_datazone_project", j[key])
-            #common.add_dependancy("aws_datazone_environment_profile", j[key])
+            common.add_dependancy("aws_datazone_glossary", j[key])
+            common.add_dependancy("aws_datazone_glossary_term", j[key])
+            common.add_dependancy("aws_datazone_environment_profile", j[key])
 
     except Exception as e:
         common.handle_error(e,str(inspect.currentframe().f_code.co_name),clfn,descfn,topkey,id)
@@ -41,6 +45,7 @@ def get_aws_datazone_project(type, id, clfn, descfn, topkey, key, filterid):
         print("--> In "+str(inspect.currentframe().f_code.co_name)+" doing " + type + ' with id ' + str(id) +
               " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
     try:
+        pkey=type+"."+id
         response = []
         client = boto3.client(clfn)
 
@@ -52,12 +57,15 @@ def get_aws_datazone_project(type, id, clfn, descfn, topkey, key, filterid):
             for page in paginator.paginate(domainIdentifier=id):
                 response = response + page[topkey]
 
-        if response == []: print("Empty response for "+type+ " id="+str(id)+" returning"); return True
-        for j in response:
-            pkey=id+":"+j[key]
-            common.write_import(type,pkey,None) 
-            pkey=type+"."+id
+        if response == []: 
+            print("Empty response for "+type+ " id="+str(id)+" returning") 
             globals.rproc[pkey]=True
+            return True
+        for j in response:
+            theid=id+":"+j[key]
+            common.write_import(type,theid,None) 
+            
+        globals.rproc[pkey]=True
 
     except Exception as e:
         common.handle_error(e,str(inspect.currentframe().f_code.co_name),clfn,descfn,topkey,id)
@@ -71,26 +79,30 @@ def get_aws_datazone_glossary(type, id, clfn, descfn, topkey, key, filterid):
         print("--> In "+str(inspect.currentframe().f_code.co_name)+" doing " + type + ' with id ' + str(id) +
               " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
     try:
+        pkey=type+"."+id
         response = []
         client = boto3.client(clfn)
 
         paginator = client.get_paginator(descfn)
         if id is None:
-            print("WARNING must pass domain id to get_aws_datazone_project")
+            print("WARNING must pass domain id to get_aws_datazone_glossary")
             return True
         else:
             for page in paginator.paginate(domainIdentifier=id,searchScope='GLOSSARY'):
                 response = response + page[topkey]
 
-        if response == []: print("Empty response for "+type+ " id="+str(id)+" returning"); return True
+        if response == []: 
+            print("Empty response for "+type+ " id="+str(id)+" returning")
+            globals.rproc[pkey]=True
+            return True
         #print(str(response))
         for k in response:
             j=k['glossaryItem']
             theid=id+","+j[key]+","+j['owningProjectId']
             print(theid)
             common.write_import(type,theid,None) 
-            pkey=type+"."+id
-            globals.rproc[pkey]=True
+
+        globals.rproc[pkey]=True
 
     except Exception as e:
         common.handle_error(e,str(inspect.currentframe().f_code.co_name),clfn,descfn,topkey,id)
@@ -102,26 +114,31 @@ def get_aws_datazone_glossary_term(type, id, clfn, descfn, topkey, key, filterid
         print("--> In "+str(inspect.currentframe().f_code.co_name)+" doing " + type + ' with id ' + str(id) +
               " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
     try:
+        pkey=type+"."+id
         response = []
         client = boto3.client(clfn)
 
         paginator = client.get_paginator(descfn)
         if id is None:
-            print("WARNING must pass domain id to get_aws_datazone_project")
+            print("WARNING must pass domain id to get_aws_datazone_glossary_term")
             return True
         else:
             for page in paginator.paginate(domainIdentifier=id,searchScope='GLOSSARY_TERM'):
                 response = response + page[topkey]
 
-        if response == []: print("Empty response for "+type+ " id="+str(id)+" returning"); return True
+        if response == []: 
+            print("Empty response for "+type+ " id="+str(id)+" returning")
+            globals.rproc[pkey]=True    
+            return True
         #print(str(response))
         for k in response:
             j=k['glossaryTermItem']
             theid=id+","+j[key]+","+j['glossaryId']
             print(theid)
             common.write_import(type,theid,None) 
-            pkey=type+"."+id
-            globals.rproc[pkey]=True
+        globals.rproc[pkey]=True
+        
+        
 
     except Exception as e:
         common.handle_error(e,str(inspect.currentframe().f_code.co_name),clfn,descfn,topkey,id)
@@ -133,19 +150,23 @@ def get_aws_datazone_form_type(type, id, clfn, descfn, topkey, key, filterid):
         print("--> In "+str(inspect.currentframe().f_code.co_name)+" doing " + type + ' with id ' + str(id) +
               " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
     try:
+        pkey=type+"."+id
         response = []
         client = boto3.client(clfn)
 
         paginator = client.get_paginator(descfn)
         if id is None:
-            print("WARNING must pass domain id to get_aws_datazone_project")
+            print("WARNING must pass domain id to get_aws_datazone_form_type")
             return True
         else:
             for page in paginator.paginate(domainIdentifier=id,managed=False,
                         searchScope='FORM_TYPE',sort={'attribute': 'name','order': 'ASCENDING'}):
                 response = response + page[topkey]
 
-        if response == []: print("Empty response for "+type+ " id="+str(id)+" returning"); return True
+        if response == []: 
+            print("Empty response for "+type+ " id="+str(id)+" returning")
+            globals.rproc[pkey]=True
+            return True
         print(str(response))
         for k in response:
 
@@ -153,8 +174,8 @@ def get_aws_datazone_form_type(type, id, clfn, descfn, topkey, key, filterid):
             theid=id+","+j['name']+","+j['revision']
             print(theid)
             common.write_import(type,theid,None) 
-            pkey=type+"."+id
-            globals.rproc[pkey]=True
+
+        globals.rproc[pkey]=True
 
     except Exception as e:
         common.handle_error(e,str(inspect.currentframe().f_code.co_name),clfn,descfn,topkey,id)
@@ -168,23 +189,27 @@ def get_aws_datazone_environment_blueprint_configuration(type, id, clfn, descfn,
         print("--> In "+str(inspect.currentframe().f_code.co_name)+" doing " + type + ' with id ' + str(id) +
               " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
     try:
+        pkey=type+"."+id
         response = []
         client = boto3.client(clfn)
 
         paginator = client.get_paginator(descfn)
         if id is None:
-            print("WARNING must pass domain id to get_aws_datazone_project")
+            print("WARNING must pass domain id to get_aws_datazone_environment_blueprint_configuration")
             return True
         else:
             for page in paginator.paginate(domainIdentifier=id):
                 response = response + page[topkey]
 
-        if response == []: print("Empty response for "+type+ " id="+str(id)+" returning"); return True
+        if response == []: 
+            print("Empty response for "+type+ " id="+str(id)+" returning")
+            globals.rproc[pkey]=True
+            return True
         for j in response:
             theid=id+'/'+j[key]
             common.write_import(type,theid,None) 
-            pkey=type+"."+id
-            globals.rproc[pkey]=True
+            
+        globals.rproc[pkey]=True
 
     except Exception as e:
         common.handle_error(e,str(inspect.currentframe().f_code.co_name),clfn,descfn,topkey,id)
@@ -201,7 +226,7 @@ def get_aws_datazone_environment_profile(type, id, clfn, descfn, topkey, key, fi
 
         paginator = client.get_paginator(descfn)
         if id is None:
-            print("WARNING must pass domain id to get_aws_datazone_project")
+            print("WARNING must pass domain id to get_aws_datazone_environment_profile")
             return True
         else:
             for page in paginator.paginate(domainIdentifier=id):

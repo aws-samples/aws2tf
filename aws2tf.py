84abab3f28c6a6a63847e69551a326cfc8e50f8d
Andy Thomas <awsandy@amazon.co.uk>
2024-08-20 21:53:22 +0100

    up

--- aws2tf.py
+++ aws2tf.py
@@ -140,11 +140,12 @@ if __name__ == '__main__':
     #os.environ["AWS"] = "aws --region "+region+" "
  
     # get the current env and set directory
-    if globals.debug: print("setting sesison region="+region)
+    if globals.debug: print("setting session region="+region)
     try:
         my_session = boto3.setup_default_session(region_name=region)
     except Exception as e: 
         print("AWS Authorization Error: "+str(e))
+    if globals.debug: print("getting account")
     try:
         globals.acc = boto3.client('sts').get_caller_identity().get('Account')
     except Exception as e:

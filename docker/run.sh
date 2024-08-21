b8bb8f6eedc0d2ae0df3ead62558d37c8c9a51ca
Andy Thomas <awsandy@amazon.co.uk>
2024-08-21 09:15:52 +0100

    up

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,6 @@
 docker run  --name aws2tf --rm -v $(pwd):/aws2tf aws2tf -t vpc -v 
-docker run  --name aws2tf --rm -v $(pwd)/generated:/aws2tf/generated aws2tf
-docker run  --name aws2tf -d -t -v $(pwd):/aws2tf aws2tf 
+docker run  --name aws2tf --rm -v $(pwd)/generated:/aws2tf/generated aws2tf 
+docker run  --name aws2tf -d -t -v $(pwd)/generated:/aws2tf/generated -v ~/.aws:/root/.aws aws2tf
 #docker logs -t aws2tf
 docker exec -it aws2tf sh  
 docker stop  aws2tf && docker rm aws2tf

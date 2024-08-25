90504bdd9707f74a2f28dcdf8fe748ea9cbfcf21
Andy Thomas <awsandy@amazon.co.uk>
2024-08-25 21:21:30 +0100

    a stuff

--- lambda/run-test.sh
+++ lambda/run-test.sh
@@ -1,1 +1,2 @@
-docker run --platform linux/arm64 -p 9000:8080 laws2tf
+docker run --name aws2tf --platform linux/arm64 -p 9000:8080 laws2tf
+curl "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"payload":"hello world!"}'

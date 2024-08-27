fcd12ddf45ef0ea281efa81a1b336f5197da2f68
Andy Thomas <awsandy@amazon.co.uk>
2024-08-27 19:55:12 +0100

    up

--- .scripts/copy2s3.sh
+++ .scripts/copy2s3.sh
@@ -5,7 +5,7 @@ merge=$(echo $3)
 echo $acc $reg $merge
 if aws s3 ls "s3://aws2tf-${acc}_${reg}" 2>&1 | grep -q 'NoSuchBucket'; then
     echo "Bucket aws2tf-${acc}_${reg} does not exist. Creating..."
-    cmd=$(print "aws s3 mb s3://aws2tf-${acc}_${reg}")
+    cmd=$(printf "aws s3 mb s3://aws2tf-%s_%s" $acc $reg)
     eval $cmd
     if [ $? -ne 0 ]; then
         echo "Error creating bucket aws2tf-${acc}_${reg}"

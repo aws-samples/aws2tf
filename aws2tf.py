3c85251f44a9fc040a28d7d1292c9ee30cc31bb5
Andy Thomas <awsandy@amazon.co.uk>
2024-08-27 18:31:22 +0100

    up

--- aws2tf.py
+++ aws2tf.py
@@ -342,14 +342,6 @@ def main():
     #    print("No Known Dependancies")
 
     common.tfplan1()
-    if globals.serverless:
-        print("Serverless -end-")
-        #com = "cat plan1.json | jq ."
-        #print(com)
-        #rout = common.rc(com)
-        #print(rout.stdout.decode().rstrip())
-        print("Exiting with cwd:",str(os.getcwd()))
-        exit()
     common.tfplan2()
     
     if ":" in globals.rproc:

9c22b414b4e7f8f3f17863ee6c9a6c7eb01502b3
Andy Thomas <awsandy@amazon.co.uk>
2024-08-27 21:15:26 +0100

    up

--- .scripts/copy2s3.sh
+++ .scripts/copy2s3.sh
@@ -14,7 +14,7 @@ if aws s3 ls "s3://aws2tf-${acc}-${reg}" 2>&1 | grep -q 'NoSuchBucket'; then
     fi
 fi
 echo "Copying to s3://aws2tf-${acc}-${reg}"
-cmd=$(printf "aws s3 cp /tmp/aws2tf/generated/tf-%s-%s s3://aws2tf-%s-%s/ --recursive" $acc $reg $acc $reg)
+cmd=$(printf "aws s3 cp /tmp/aws2tf/generated/tf-%s-%s s3://aws2tf-%s-%s/ --recursive --exclude " $acc $reg $acc $reg)
 echo $cmd
-aws s3 cp /tmp/aws2tf/generated/tf-${acc}-${reg} s3://aws2tf-${acc}-${reg}/ --recursive
+aws s3 cp /tmp/aws2tf/generated/tf-${acc}-${reg} s3://aws2tf-${acc}-${reg}/ --recursive --exclude ".terraform/*" 
 echo "Done copying to s3"

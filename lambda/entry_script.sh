0696fc8d091b0618db7ad8931a44317f50e6084f
Andy Thomas <awsandy@amazon.co.uk>
2024-08-27 16:11:21 +0100

    up

--- lambda/entry_script.sh
+++ lambda/entry_script.sh
@@ -2,16 +2,16 @@
 echo "entry 001"
 echo "working dir"
 pwd
+cd /tmp
+cp -r /aws2tf .
 mkdir -p /tmp/aws2tf/generated
 cd /tmp/aws2tf
-cp -r /aws2tf/* .
-pwd
 echo "files in /tmp/aws2tf"
 ls
 set | grep AWS
-#echo "Starting lambda handler"
-#if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
-#    exec ~/.aws-lambda-rie/aws-lambda-rie python3 -m awslambdaric $@
-#else
-#    exec python3 -m awslambdaric $@
-#fi
+echo "Starting lambda handler"
+if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
+    exec ~/.aws-lambda-rie/aws-lambda-rie python3 -m awslambdaric $@
+else
+    exec python3 -m awslambdaric $@
+fi

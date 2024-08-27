57be290000f530e16e1ffa111c839c630741cdb5
Andy Thomas <awsandy@amazon.co.uk>
2024-08-27 15:34:58 +0100

    up

--- lambda/entry_script.sh
+++ lambda/entry_script.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 echo "working dir"
+pwd
 mkdir -p /tmp/aws2tf/generated
 cp -r * /tmp/aws2tf
+cd /tmp/aws2tf
+ls -l
 set | grep AWS
 echo "Starting lambda handler"
 if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then

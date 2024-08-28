53175f5b378ba252471f49ae6977cc5f63958e9a
Andy Thomas <awsandy@amazon.co.uk>
2024-08-28 14:28:27 +0100

    update readme

--- copy-to-aws2tf.sh
+++ copy-to-aws2tf.sh
@@ -1,7 +1,6 @@
-cp aws2tf.py README.md Stack*.md Terraform*.md ../aws2tf
+cp aws2tf.py README*.md Stack*.md Terraform*.md ../aws2tf
 cp .python/*.py ../aws2tf/.python
 cp .scripts/*.sh ../aws2tf/.scripts
 cp -r .python/fixtf_aws_resources ../aws2tf/.python
 cp -r .python/get_aws_resources ../aws2tf/.python
-cp -r README-docker.md ../aws2tf/README-docker.md
 cp requirements.txt ../aws2tf

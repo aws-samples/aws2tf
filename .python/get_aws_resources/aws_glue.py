fadae1d5ff28b544daad815db1fa6608da4e53ab
Andy Thomas <awsandy@amazon.co.uk>
2024-09-01 15:40:48 +0100

    fix aws_glue_workflow

--- .python/get_aws_resources/aws_glue.py
+++ .python/get_aws_resources/aws_glue.py
@@ -444,3 +444,31 @@ def get_aws_glue_data_quality_ruleset(type, id, clfn, descfn, topkey, key, filte
         common.handle_error(e,str(inspect.currentframe().f_code.co_name),clfn,descfn,topkey,id)
 
     return True
+
+# aws_glue_workflow
+def get_aws_glue_workflow(type, id, clfn, descfn, topkey, key, filterid):
+    if globals.debug:
+        print("--> In "+str(inspect.currentframe().f_code.co_name)+" doing " + type + ' with id ' + str(id) +
+              " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
+    try:
+        response = []
+        client = boto3.client(clfn)
+        if id is None:
+            paginator = client.get_paginator(descfn)
+            for page in paginator.paginate():
+                response = response + page[topkey]
+            if response == []: print("Empty response for "+type+ " id="+str(id)+" returning"); return True
+            for j in response:
+                print(j)
+                common.write_import(type, j, None)
+
+        else:
+            response = client.get_workflow(Name=id)
+            if response == []: print("Empty response for "+type+ " id="+str(id)+" returning"); return True
+            j=response['Workflow']
+            common.write_import(type, j[key], None)
+
+    except Exception as e:
+        common.handle_error(e, str(inspect.currentframe().f_code.co_name), clfn, descfn, topkey, id)
+
+    return True

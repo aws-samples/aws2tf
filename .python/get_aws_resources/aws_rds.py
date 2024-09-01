e4e48ee61e3f21389be8687d43764b162bc741b6
Andy Thomas <awsandy@amazon.co.uk>
2024-09-01 15:23:36 +0100

    rds cluster fix

--- .python/get_aws_resources/aws_rds.py
+++ .python/get_aws_resources/aws_rds.py
@@ -224,8 +224,12 @@ def get_aws_rds_cluster_instance(type, id, clfn, descfn, topkey, key, filterid):
                 engine=j['Engine']
                 if engine.startswith("aurora"): 
                     common.write_import(type, j[key], None)
+                    pkey=type+"."+j[key]
+                    globals.rproc[pkey]=True
+                    
                 else:
                     continue
+            
 
         else:
             response = client.describe_db_instances(DBInstanceIdentifier=id)
@@ -235,6 +239,49 @@ def get_aws_rds_cluster_instance(type, id, clfn, descfn, topkey, key, filterid):
                 engine=j['Engine']
                 if engine.startswith("aurora"):
                     common.write_import(type, j[key], None)
+                    pkey=type+"."+j[key]
+                    globals.rproc[pkey]=True
+                else:
+                    continue
+
+    except Exception as e:
+        common.handle_error(e, str(inspect.currentframe().f_code.co_name), clfn, descfn, topkey, id)
+
+    return True
+
+def get_aws_rds_cluster(type, id, clfn, descfn, topkey, key, filterid):
+    if globals.debug:
+        print("--> In "+str(inspect.currentframe().f_code.co_name)+" doing " + type + ' with id ' + str(id) +
+              " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
+    try:
+        response = []
+        client = boto3.client(clfn)
+        if id is None:
+            paginator = client.get_paginator(descfn)
+            for page in paginator.paginate():
+                response = response + page[topkey]
+            if response == []: print("Empty response for "+type+ " id="+str(id)+" returning"); return True
+            for j in response: 
+                engine=j['Engine']
+                if engine.startswith("aurora"): 
+                    common.write_import(type, j[key], None)
+                    pkey=type+"."+j[key]
+                    globals.rproc[pkey]=True
+                    
+                else:
+                    continue
+            
+
+        else:
+            response = client.describe_db_instances(DBInstanceIdentifier=id)
+            if response == []: print("Empty response for "+type+ " id="+str(id)+" returning"); return True
+            for j in response[topkey]:
+                #print(j)
+                engine=j['Engine']
+                if engine.startswith("aurora"):
+                    common.write_import(type, j[key], None)
+                    pkey=type+"."+j[key]
+                    globals.rproc[pkey]=True
                 else:
                     continue
 

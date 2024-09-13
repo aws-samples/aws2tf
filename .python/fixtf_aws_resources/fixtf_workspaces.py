dfdea55f289b6ed47c94e53bdd38eb2d15ad3fc0
Andy Thomas <awsandy@amazon.co.uk>
2024-09-13 15:26:47 +0100

    up

--- .python/fixtf_aws_resources/fixtf_workspaces.py
+++ .python/fixtf_aws_resources/fixtf_workspaces.py
@@ -5,6 +5,9 @@ def aws_workspaces_connection_alias(t1,tt1,tt2,flag1,flag2):
 
 def aws_workspaces_directory(t1,tt1,tt2,flag1,flag2):
 	skip=0
+	if tt1=="directory_id":
+		t1=tt1+" = aws_directory_service_directory."+tt2+".id\n"
+		common.add_dependancy("aws_directory_service_directory",tt2)
 	return skip,t1,flag1,flag2
 
 def aws_workspaces_ip_group(t1,tt1,tt2,flag1,flag2):
@@ -14,7 +17,7 @@ def aws_workspaces_ip_group(t1,tt1,tt2,flag1,flag2):
 def aws_workspaces_workspace(t1,tt1,tt2,flag1,flag2):
 	skip=0
 	if tt1=="directory_id":
+		t1=tt1+" = aws_workspaces_directory."+tt2+".id\n"
 		common.add_dependancy("aws_workspaces_directory",tt2)
-		common.add_dependancy("aws_directory_service_directory",tt2)
 	return skip,t1,flag1,flag2
 

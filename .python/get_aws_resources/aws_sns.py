2c252d8a40db6b6d30443df60f4eb69bd33e8c9f
Andy Thomas <awsandy@amazon.co.uk>
2024-10-04 20:37:55 +0100

    bug fixes

--- .python/get_aws_resources/aws_sns.py
+++ .python/get_aws_resources/aws_sns.py
@@ -37,7 +37,6 @@ def get_aws_sns_topic_policy(type, id, clfn, descfn, topkey, key, filterid):
         print("--> In "+str(inspect.currentframe().f_code.co_name)+" doing " + type + ' with id ' + str(id) +
               " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
     try:
-
         response = []
         client = boto3.client(clfn)
         if id is None:
@@ -45,10 +44,15 @@ def get_aws_sns_topic_policy(type, id, clfn, descfn, topkey, key, filterid):
             return True
 
         else:
-             
             response = client.get_topic_attributes(TopicArn=id)
-            if response == []: print("Empty response for "+type+ " id="+str(id)+" returning"); return True
+            if response == []: 
+                print("Empty response for "+type+ " id="+str(id)+" returning"); 
+                pkey="aws_sns_topic_policy."+id
+                globals.rproc[pkey]=True
+                return True
             common.write_import(type,id,None)
+            pkey="aws_sns_topic_policy."+id
+            globals.rproc[pkey]=True
 
     except Exception as e:
         common.handle_error(e,str(inspect.currentframe().f_code.co_name),clfn,descfn,topkey,id)

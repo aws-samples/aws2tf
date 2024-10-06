eeede6bbd93f517e7dc542e7b447a3b394dbed46
awsandy <awsandy@amazon.co.uk>
2024-10-06 11:38:12 +0100

    readonly fixes

--- .python/fixtf_aws_resources/fixtf_glue.py
+++ .python/fixtf_aws_resources/fixtf_glue.py
@@ -1,18 +1,31 @@
 import globals 
 import common
+import fixtf
+import base64
+import boto3
+import sys
+import os
+import inspect
 
 def aws_glue_crawler(t1,tt1,tt2,flag1,flag2):
 	skip=0
-	if tt1 == "database_name" and tt2 != "null":
-		if globals.gluedbs[tt2]:
-			t1 = tt1 + " = aws_glue_catalog_database.d-"+globals.acc+"__"+tt2+".name\n"
-		#common.add_dependancy("aws_glue_catalog_database",tt2)
+	
+	try:
+		if tt1 == "database_name" and tt2 != "null":
+			if tt2 in str(globals.gluedbs):
+				t1 = tt1 + " = aws_glue_catalog_database.d-"+globals.acc+"__"+tt2+".name\n"
+			#common.add_dependancy("aws_glue_catalog_database",tt2)
 
-	elif tt1 == "sample_size":
-		if tt2 == "0": skip=1
-	elif tt1 == "security_configuration" and tt2 != "null":
-		t1 = tt1 + " = aws_glue_security_configuration."+tt2+".id\n"
-		common.add_dependancy("aws_glue_security_configuration",tt2)
+		elif tt1 == "sample_size":
+			if tt2 == "0": skip=1
+		elif tt1 == "security_configuration" and tt2 != "null":
+			t1 = tt1 + " = aws_glue_security_configuration."+tt2+".id\n"
+			common.add_dependancy("aws_glue_security_configuration",tt2)
+	except Exception as e:
+		print(e)
+		print("fixtf_glue.py aws_glue_crawler Exception=", str(e))
+		print("fixtf_glue.py t1=", t1)
+	
 	return skip,t1,flag1,flag2
 
 def aws_glue_catalog_database(t1,tt1,tt2,flag1,flag2):

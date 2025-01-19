62dabe3b08427ba6601ff81d38b8d1ad45f49f4a
Andy Thomas <awsandy@amazon.co.uk>
2025-01-19 16:37:42 +0000

    up

--- copy-to-aws2tf.sh
+++ copy-to-aws2tf.sh
@@ -1,5 +1,5 @@
 cp aws2tf.py README*.md Stack*.md Terraform*.md ../aws2tf
-cp .python/*.py ../aws2tf/.python
-cp -r .python/fixtf_aws_resources ../aws2tf/.python
-cp -r .python/get_aws_resources ../aws2tf/.python
+cp code/*.py ../aws2tf/code
+cp -r code/fixtf_aws_resources ../aws2tf/code
+cp -r code/get_aws_resources ../aws2tf/code
 cp requirements.txt ../aws2tf

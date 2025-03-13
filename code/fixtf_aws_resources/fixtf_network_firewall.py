c8fe673928226c6b90e075deb368313972495d56
Andy Thomas <awsandy@amazon.co.uk>
2025-03-13 21:58:24 +0000

    firewall rules

--- code/fixtf_aws_resources/fixtf_network_firewall.py
+++ code/fixtf_aws_resources/fixtf_network_firewall.py
@@ -11,6 +11,10 @@ def aws_networkfirewall_firewall(t1,tt1,tt2,flag1,flag2):
 def aws_networkfirewall_firewall_policy(t1,tt1,tt2,flag1,flag2):
 	skip=0
 	if tt1=="priority" and tt2=="0": skip=1
+	elif tt1=="resource_arn" and tt2!="null" and tt2.startswith("arn:"):
+		tarn=tt2.replace("/", "_").replace(".", "_").replace(":", "_").replace("|", "_").replace("$", "_").replace(", ", "_").replace("&", "_").replace("#", "_").replace("[", "_").replace("]", "_").replace("=", "_").replace("!", "_").replace(";", "_")
+		t1=tt1 + " = aws_networkfirewall_rule_group." + tarn + ".arn\n"
+		common.add_dependancy("aws_networkfirewall_rule_group", tt2)
 	return skip,t1,flag1,flag2
 
 def aws_networkfirewall_logging_configuration(t1,tt1,tt2,flag1,flag2):
--- code/get_aws_resources/aws_network_firewall.py
+++ code/get_aws_resources/aws_network_firewall.py
@@ -75,3 +75,57 @@ def get_aws_networkfirewall_firewall_policy(type, id, clfn, descfn, topkey, key,
         common.handle_error(e,str(inspect.currentframe().f_code.co_name),clfn,descfn,topkey,id)
 
     return True
+
+
+def get_aws_networkfirewall_rule_group(type, id, clfn, descfn, topkey, key, filterid):
+    if globals.debug:
+        print("--> In "+str(inspect.currentframe().f_code.co_name)+" doing " + type + ' with id ' + str(id) +
+              " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
+    try:
+        response = []
+        config = Config(retries = {'max_attempts': 10,'mode': 'standard'})
+        client = boto3.client(clfn,config=config)
+        if id is None:
+            paginator = client.get_paginator(descfn)
+            for page in paginator.paginate(Type='STATELESS'):
+                response = response + page[topkey]
+            if response == []: 
+                if globals.debug: print("Empty response for "+type+ " id="+str(id)+" returning") 
+            else:    
+                for j in response:
+                    common.write_import(type,j[key],None) 
+
+            for page in paginator.paginate(Type='STATEFUL'):
+                response = response + page[topkey]
+            if response == []: 
+                if globals.debug: print("Empty response for "+type+ " id="+str(id)+" returning") 
+                return True
+            for j in response:
+                common.write_import(type,j[key],None) 
+
+        else: 
+            if id.startswith("arn:"):
+                if ":stateless-rulegroup/" in id:
+                    response = client.describe_rule_group(RuleGroupArn=id,AnalyzeRuleGroup=False,Type='STATELESS')
+            else:      
+                print("INFO: must pass arn for rule group")
+            if response == []: 
+                if globals.debug: print("Empty response for "+type+ " id="+str(id)+" returning") 
+            else:
+                j=response['RuleGroupResponse']
+                common.write_import(type, j['RuleGroupArn'], None)
+            if id.startswith("arn:"):
+                if ":stateful-rulegroup/" in id:
+                    response = client.describe_rule_group(RuleGroupArn=id,AnalyzeRuleGroup=False,Type='STATEFUL')
+            else:      
+                print("INFO: must pass arn for rule group")
+            if response == []: 
+                if globals.debug: print("Empty response for "+type+ " id="+str(id)+" returning") 
+            else:
+                j=response['RuleGroupResponse']
+                common.write_import(type, j['RuleGroupArn'], None)
+
+    except Exception as e:
+        common.handle_error(e,str(inspect.currentframe().f_code.co_name),clfn,descfn,topkey,id)
+
+    return True

000c8503f4a0b576ad27803b7b249c910c16fe6c
Andy Thomas <awsandy@amazon.co.uk>
2025-03-14 15:18:08 +0000

    network firewall support

--- code/fixtf_aws_resources/fixtf_network_firewall.py
+++ code/fixtf_aws_resources/fixtf_network_firewall.py
@@ -19,6 +19,10 @@ def aws_networkfirewall_firewall_policy(t1,tt1,tt2,flag1,flag2):
 
 def aws_networkfirewall_logging_configuration(t1,tt1,tt2,flag1,flag2):
 	skip=0
+	if tt1=="firewall_arn" and tt2!="null" and tt2.startswith("arn:"):
+		tarn=tt2.replace("/", "_").replace(".", "_").replace(":", "_").replace("|", "_").replace("$", "_").replace(", ", "_").replace("&", "_").replace("#", "_").replace("[", "_").replace("]", "_").replace("=", "_").replace("!", "_").replace(";", "_")
+		t1=tt1 + " = aws_networkfirewall_firewall." + tarn + ".arn\n"
+		common.add_dependancy("aws_networkfirewall_firewall", tt2)
 	return skip,t1,flag1,flag2
 
 def aws_networkfirewall_resource_policy(t1,tt1,tt2,flag1,flag2):

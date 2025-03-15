1124acd3a7b081ebe867e6697898762446f369c6
Andy Thomas <awsandy@amazon.co.uk>
2025-03-15 16:50:35 +0000

    up

--- code/fixtf_aws_resources/fixtf_ec2.py
+++ code/fixtf_aws_resources/fixtf_ec2.py
@@ -555,23 +555,22 @@ def  aws_route_table(t1,tt1,tt2,flag1,flag2):
 	if "cidr_block" in tt1:   
 		if tt2 == "": t1=tt1 + " = null\n"
 
-	elif "nat_gateway_id" in tt1 and tt2 != "null":
-        
-		if tt2 != "":
-			t1=tt1 + " = aws_nat_gateway." + tt2 + ".id\n"
-			common.add_dependancy("aws_nat_gateway",tt2)
-	elif tt1 == "gateway_id" and tt2 != "null":
-        
-		if tt2 != "":
-			t1=tt1 + " = aws_internet_gateway." + tt2 + ".id\n"
-			common.add_dependancy("aws_internet_gateway",tt2)
-	elif tt1 == "vpc_peering_connection_id" and tt2 != "null":
-        
-		if tt2.startswith("pcx-"):
-			t1=tt1 + " = aws_vpc_peering_connection." + tt2 + ".id\n"
-			common.add_dependancy("aws_vpc_peering_connection",tt2)
-
-
+	elif "nat_gateway_id" in tt1 and tt2.startswith("nat-"):
+		t1=tt1 + " = aws_nat_gateway." + tt2 + ".id\n"
+		common.add_dependancy("aws_nat_gateway",tt2)
+	elif tt1 == "gateway_id" and tt2.startswith("igw-"):
+		t1=tt1 + " = aws_internet_gateway." + tt2 + ".id\n"
+		common.add_dependancy("aws_internet_gateway",tt2)
+	elif tt1 == "vpc_peering_connection_id" and tt2.startswith("pcx-"):
+		t1=tt1 + " = aws_vpc_peering_connection." + tt2 + ".id\n"
+		common.add_dependancy("aws_vpc_peering_connection",tt2)
+	elif tt1 == "transit_gateway_id" and tt2.startswith("tgw-"):
+		t1=tt1 + " = aws_ec2_transit_gateway." + tt2 + ".id\n"
+		common.add_dependancy("aws_ec2_transit_gateway", tt2)
+# network_interface_id
+	elif tt1 == "network_interface_id" and tt2.startswith("eni-"):
+		t1=tt1 + " = aws_network_interface." + tt2 + ".id\n"
+		common.add_dependancy("aws_network_interface", tt2)
 
 	return skip,t1,flag1,flag2
 
--- code/get_aws_resources/aws_ec2.py
+++ code/get_aws_resources/aws_ec2.py
@@ -1260,3 +1260,28 @@ def get_aws_vpc_peering_connection(type, id, clfn, descfn, topkey, key, filterid
 
     return True
 
+# aws_network_interface
+def get_aws_network_interface(type, id, clfn, descfn, topkey, key, filterid):
+    if globals.debug:
+        print("--> In "+str(inspect.currentframe().f_code.co_name)+" doing " + type + ' with id ' + str(id) +
+              " clfn="+clfn+" descfn="+descfn+" topkey="+topkey+" key="+key+" filterid="+filterid)
+    try:
+        response = []
+        client = boto3.client(clfn)
+        if id is None:
+            print("WARNING: "+type+" id is None - must pass eni-xxxxxxxxxx as paramter returning")
+            return True
+
+        if id.startswith("eni-"):
+            response = client.describe_network_interfaces(NetworkInterfaceIds=[id])
+            if response == []:
+                if globals.debug: print("Empty response for "+type+ " id="+str(id)+" returning")
+                return True
+            for j in response[topkey]:
+                common.write_import(type, j[key], None)
+
+    except Exception as e:
+        common.handle_error(e, str(inspect.currentframe().f_code.co_name), clfn, descfn, topkey, id)
+
+    return True
+

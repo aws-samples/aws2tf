b31ea4de7fa134dcb025f2331235c06805fe7c09
Andy Thomas <awsandy@amazon.co.uk>
2025-05-23 19:21:15 +0100

    route53 star

--- code/get_aws_resources/aws_route53.py
+++ code/get_aws_resources/aws_route53.py
@@ -14,16 +14,16 @@ def get_aws_route53_zone(type, id, clfn, descfn, topkey, key, filterid):
             paginator = client.get_paginator(descfn)
             for page in paginator.paginate():
                 response = response + page[topkey]
-            if response == []: 
+            if response == []:
                 print("Empty response for "+type+ " id="+str(id)+" returning")
                 return True
             for j in response:
-                common.write_import(type,j[key],None) 
+                common.write_import(type,j[key],None)
                 common.add_dependancy("aws_route53_record",j[key])
 
-        else:      
+        else:
             response = client.get_hosted_zone(Id=id)
-            if response['HostedZone'] == []: 
+            if response['HostedZone'] == []:
                 print("Empty response for "+type+ " id="+str(id)+" returning")
                 return True
             j=response['HostedZone']
@@ -45,7 +45,7 @@ def get_aws_route53_record(type, id, clfn, descfn, topkey, key, filterid):
         client = boto3.client(clfn)
         if id is None:
             print("WARNING: No id or invalid provided for "+type)
-        else:   
+        else:
             rkey=type+"."+id
             globals.rproc[rkey]=True
             if id.startswith("/hostedzone/"): id=id.split("/")[2]
@@ -53,7 +53,7 @@ def get_aws_route53_record(type, id, clfn, descfn, topkey, key, filterid):
             paginator = client.get_paginator(descfn)
             for page in paginator.paginate(HostedZoneId=id):
                 response = response + page[topkey]
-            if response == []: 
+            if response == []:
                 print("Empty response for "+type+ " id="+str(id)+" returning")
                 return True
             for j in response:
@@ -62,8 +62,11 @@ def get_aws_route53_record(type, id, clfn, descfn, topkey, key, filterid):
                 r53type=j['Type']
                 if r53name.endswith("."): r53name=r53name[:-1]
                 if r53type=="A":
-                    pkey=id+"_"+r53name+"_"+r53type
-                    common.write_import(type,pkey,None) 
+                    import_id = id+"_"+r53name+"_"+r53type
+                    import_id = import_id.replace("\\052", "*")
+                    resource_name = id+"_"+r53name.replace("*", "star").replace("\\052", "star")+"_"+r53type
+
+                    common.write_import(type, import_id, resource_name)
 
     except Exception as e:
         common.handle_error(e,str(inspect.currentframe().f_code.co_name),clfn,descfn,topkey,id)

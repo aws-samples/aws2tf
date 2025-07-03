7686f2342be9c15dfde0cb413ddbebffc954ea57
Andy Thomas <awsandy@amazon.co.uk>
2025-07-03 15:51:18 +0100

    msk configuration+_info

--- code/fixtf_aws_resources/fixtf_kafka.py
+++ code/fixtf_aws_resources/fixtf_kafka.py
@@ -21,6 +21,9 @@ def aws_msk_cluster(t1,tt1,tt2,flag1,flag2):
 		# pass the arn
 		common.add_dependancy("aws_msk_configuration", tt2)
 	elif tt1=="volume_throughput" and tt2=="0": skip=1
+	elif tt1=="cluster_name":
+		t1=t1+ "\nlifecycle {\n" + "   ignore_changes = [configuration_info]\n" +  "}\n"
+
 	#elif tt1=="arn" and tt2!="null":
 	#	t1=tt1+" = aws_msk_configuration."+tarn+".arn\n"
 

f8c31b6dad724ded8e05385c333a8422698626e9
Andy Thomas <awsandy@amazon.co.uk>
2025-12-26 14:07:08 +0000

    up

--- code/fixtf_aws_resources/fixtf_glue.py
+++ code/fixtf_aws_resources/fixtf_glue.py
@@ -83,6 +83,7 @@ def aws_glue_job(t1,tt1,tt2,flag1,flag2):
 	if tt1=="script_location" and tt2.endswith(".py"):
 		com="aws s3 cp "+tt2+" ."
 		log.info("executing: "+com)
+		log.info("aws_glue_job t1= %s", t1)
 		rout = common.rc(com) 
 	
 	return skip,t1,flag1,flag2
